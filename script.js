6719254dcda54d5ddb80ab5bece406cf1838e2aa
Sara Agnestrand <sara@agnestrand.net>
2022-11-11 11:21:43 +0100

    new code

---
+++ script.js
@@ -0,0 +1,71 @@
+
+//Colorama
+
+// const input = document.querySelector("input");
+// const btn = document.querySelector("button");
+// const div = document.querySelector("divn");
+
+// const validColors = ["crimsom", blue,green]
+
+// btn.onclick = () => {
+//     if(validColors.includes(input.value)){
+//         document.body.style.backgroundColor = input.value;
+//         localStorage.setItem("color", input.value)
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+//Strängpoesi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
